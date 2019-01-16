28c1906fc4710e0d65dbd53ac45d7afd639b4b9c
Umberto <Umbertogotti1979@gmail.com>
2019-01-16 23:16:28 +0100

    Code for the barrier completed.

---
+++ Barrier/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Barrier
+{
+    class Program
+    {
+        static private System.Threading.Barrier _barrier = new System.Threading.Barrier(2, b =>
+        {
+            Console.WriteLine($"Phase : {b.CurrentPhaseNumber} is finished.");
+        });
+
+        private static void Water()
+        {
+            Console.WriteLine("Putting the kattle on (takes a while....)");
+            Thread.Sleep(2000);
+            _barrier.SignalAndWait();
+            Console.WriteLine("Pouring water into the cup.");
+            _barrier.SignalAndWait();
+            Console.WriteLine("Putting the kettle away...");
+        }
+
+        private static void Cup()
+        {
+            Console.WriteLine("Finding a nice cup...");
+            _barrier.SignalAndWait();
+            Console.WriteLine("Adding tea.");
+            _barrier.SignalAndWait();
+            Console.WriteLine("Adding sugar.");
+        }
+
+        static void Main(string[] args)
+        {
+            var water = Task.Factory.StartNew(Water);
+            var cup = Task.Factory.StartNew(Cup);
+
+            var tea = Task.Factory.ContinueWhenAll(new[] {water, cup}, tasks =>
+            {
+                Console.WriteLine("Enjoy your cup of tea.");
+            });
+
+            Console.ReadKey();
+            Console.WriteLine("Finished.");
+        }
+    }
+}
